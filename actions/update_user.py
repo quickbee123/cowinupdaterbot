8f07ae61a11693efbe94b512d513c55ffe2da845
quickbee123 <rohitrobin111@gmail.com>
2021-06-20 20:15:52 +0530

    session key error corrected

--- actions/update_user.py
+++ actions/update_user.py
@@ -111,17 +111,18 @@ def filter_centers(data,districts,pincodes,pref):
 
     (age,dose,vaccine,fee) = pref
 
-    for center in centers:
-        if fee==0 or (fee==1 and center["fee_type"]=="Free") or (fee==2 and center["fee_type"]=="Paid"):
-            for session in center["sessions"]:
-                if age==0 or (age==1 and session["min_age_limit"]==18) or (age==2 and (session["min_age_limit"]==40 or session["min_age_limit"]==45)):
-                    if vaccine==0 or (vaccine==1 and session["vaccine"]=="COVISHIELD") or (vaccine==2 and session["vaccine"]=="COVAXIN") or (vaccine==3 and session["vaccine"]=="SPUTNIK"):
-                        if (dose==0 and (session["available_capacity_dose1"]>0 or session["available_capacity_dose2"]>0)) or (dose==1 and session["available_capacity_dose1"]>0) or (dose==2 and session["available_capacity_dose2"]>0):
-                            new_center = center
-                            del new_center["sessions"]
-                            new_center["session"] = session
-                            filtered_centers.append(new_center)
-                            break
+    if centers:
+        for center in centers:
+            if fee==0 or (fee==1 and center["fee_type"]=="Free") or (fee==2 and center["fee_type"]=="Paid"):
+                for session in center["sessions"]:
+                    if age==0 or (age==1 and session["min_age_limit"]==18) or (age==2 and (session["min_age_limit"]==40 or session["min_age_limit"]==45)):
+                        if vaccine==0 or (vaccine==1 and session["vaccine"]=="COVISHIELD") or (vaccine==2 and session["vaccine"]=="COVAXIN") or (vaccine==3 and session["vaccine"]=="SPUTNIK"):
+                            if (dose==0 and (session["available_capacity_dose1"]>0 or session["available_capacity_dose2"]>0)) or (dose==1 and session["available_capacity_dose1"]>0) or (dose==2 and session["available_capacity_dose2"]>0):
+                                new_center = center
+                                del new_center["sessions"]
+                                new_center["session"] = session
+                                filtered_centers.append(new_center)
+                                break
 
     return filtered_centers                    
 
